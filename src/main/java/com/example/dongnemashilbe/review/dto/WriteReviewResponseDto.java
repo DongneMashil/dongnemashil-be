eb7c3682d44d76332741a1b415aedb35dbee26a7
junyoung93 <jun@bagjun-yeong-ui-MacBookAir.local>
2023-08-05 14:13:37 +0900

    edit : 프론트와 협의 후 수정완료#19

--- src/main/java/com/example/dongnemashilbe/review/dto/WriteReviewResponseDto.java
+++ src/main/java/com/example/dongnemashilbe/review/dto/WriteReviewResponseDto.java
@@ -7,24 +7,10 @@ import lombok.Getter;
 public class WriteReviewResponseDto {
 
     private Long id;
-    private String title;
-    private String nickname;
-    private String content;
-    private String imgUrl;
-    private String videoUrl;
-    private String roadName;
-    private String tag;
-    private String address;
+
 
     public WriteReviewResponseDto(Review review) {
         this.id = review.getId();
-        this.title = review.getTitle();
-        this.content = review.getContent();
-        this.imgUrl = review.getImgUrl();
-        this.videoUrl = review.getVideoUrl();
-        this.address = review.getAddress();
-        this.nickname = review.getUser().getNickname();
-        this.roadName = review.getRoadName();
-        this.tag = review.getTag();
+
     }
 }
