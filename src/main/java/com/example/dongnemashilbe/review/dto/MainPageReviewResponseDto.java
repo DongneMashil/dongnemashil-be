da9aab53b4454bc0e2f48f1e07f73c9714fc5ce1
junyoung93 <jun@bagjun-yeong-ui-MacBookAir.local>
2023-08-09 20:52:48 +0900

    edit : 리뷰 글작성 수정

--- src/main/java/com/example/dongnemashilbe/review/dto/MainPageReviewResponseDto.java
+++ src/main/java/com/example/dongnemashilbe/review/dto/MainPageReviewResponseDto.java
@@ -11,12 +11,11 @@ public class MainPageReviewResponseDto {
     private Long id;
     private String roadName;
     private String mainImgUrl;
-    private String videoUrl;
     private String profileImgUrl;
     private Integer likeCnt;
     private boolean likebool;
 
-    public MainPageReviewResponseDto(Review review, Integer likeCount, String mainImgUrl, String videoUrl, boolean likebool){
+    public MainPageReviewResponseDto(Review review, Integer likeCount, String mainImgUrl, boolean likebool){
         this.id = review.getId();
         this.roadName = review.getRoadName();
         this.likeCnt = likeCount;
@@ -24,17 +23,6 @@ public class MainPageReviewResponseDto {
 
         this.mainImgUrl = mainImgUrl;
         this.likebool = likebool;
-        this.videoUrl = videoUrl;
 
-    }
-
-    public MainPageReviewResponseDto(Review review, Integer likeCount, boolean likebool){
-        this.id = review.getId();
-        this.roadName = review.getRoadName();
-        this.mainImgUrl = mainImgUrl;
-        this.videoUrl = videoUrl;
-        this.likeCnt = likeCount;
-        this.profileImgUrl = review.getProfileImgUrl();
-        this.likebool = likebool;
     }
 }
