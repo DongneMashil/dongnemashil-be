b4f34bd1347fda928e48625b585938549eb8d30f
codegyeon <99954094+codegyeon@users.noreply.github.com>
2023-08-15 16:32:21 +0900

    edit : 스웨거 cors 설정

--- src/main/java/com/example/dongnemashilbe/DongnemashilBeApplication.java
+++ src/main/java/com/example/dongnemashilbe/DongnemashilBeApplication.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class DongnemashilBeApplication {
 
     public static void main(String[] args) {
