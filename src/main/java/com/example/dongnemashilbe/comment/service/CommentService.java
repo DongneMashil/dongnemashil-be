eebe38ecc4538aa4dfecb909bbb4bb63025e8fec
codegyeon <99954094+codegyeon@users.noreply.github.com>
2023-08-15 09:56:44 +0900

    refactor : 댓글기능 리팩토링#43 (#45)

--- src/main/java/com/example/dongnemashilbe/comment/service/CommentService.java
+++ src/main/java/com/example/dongnemashilbe/comment/service/CommentService.java
@@ -12,10 +12,13 @@ import com.example.dongnemashilbe.global.dto.SuccessMessageDto;
 import com.example.dongnemashilbe.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -27,13 +30,18 @@ public class CommentService {
 
     public Page<CommentResponseDto> getCommentList(Long review_id, Integer page, Integer size) {
         Pageable pageable = PageRequest.of(page - 1, size);
+        Review review = getReview(review_id);
 
-        return commentRepository.findAllByReviewId(review_id,pageable).map(CommentResponseDto::new);
+        List<CommentResponseDto> commentResponseDtoList =
+                review.getCommentList().stream().map(CommentResponseDto::new).toList();
+
+        return new PageImpl(commentResponseDtoList,pageable,commentResponseDtoList.size());
     }
 
 
     public SuccessMessageDto writeComment(Long review_id, User user, CommentRequestDto commentRequestDto) {
-        Review review = reviewRepository.findById(review_id).orElseThrow(() -> new CustomException(ErrorCode.REVIEW_NOT_EXIST));
+        Review review = getReview(review_id);
+
         Comment comment = new Comment(commentRequestDto.getComment(),review,user);
         commentRepository.save(comment);
 
@@ -42,24 +50,43 @@ public class CommentService {
 
     @Transactional
     public SuccessMessageDto updateComment(User user, CommentRequestDto commentRequestDto, Long comment_id) {
-     Comment comment = commentRepository.findById(comment_id).orElseThrow(
-             ()-> new CustomException(ErrorCode.COMMENT_NOT_EXIST));
-     if(!user.getNickname().equals(comment.getUser().getNickname())){
-         throw new CustomException(ErrorCode.NOT_THE_AUTHOR);
-     }
-     comment.update(commentRequestDto.getComment());
+        Comment comment = getComment(comment_id);
+
+        validateNickname(user.getNickname(), comment.getUser().getNickname());
+
+        comment.update(commentRequestDto.getComment());
 
         return new SuccessMessageDto("댓글 수정이 완료되었습니다.");
     }
 
     public SuccessMessageDto deleteComment(User user, Long comment_id) {
-        Comment comment = commentRepository.findById(comment_id).orElseThrow(
-                ()-> new CustomException(ErrorCode.COMMENT_NOT_EXIST));
-        if(!user.getNickname().equals(comment.getUser().getNickname())){
-            throw new CustomException(ErrorCode.NOT_THE_AUTHOR);
-        }
+        Comment comment = getComment(comment_id);
+
+        validateNickname(user.getNickname(), comment.getUser().getNickname());
+
         commentRepository.delete(comment);
 
         return new SuccessMessageDto("댓글 삭제가 완료되었습니다.");
+    }
+
+    /*=============================================메서드=============================================================*/
+
+    private Review getReview(Long reviewId){
+        Review review = reviewRepository.findById(reviewId).orElseThrow(()
+                -> new CustomException(ErrorCode.REVIEW_NOT_EXIST));
+        return review;
+    }
+
+    private Comment getComment(Long commentId){
+        Comment comment = commentRepository.findById(commentId).orElseThrow(
+                ()-> new CustomException(ErrorCode.COMMENT_NOT_EXIST));
+
+        return comment;
+    }
+
+    private void validateNickname(String userNickname , String commentNickname){
+        if(!userNickname.equals(commentNickname)){
+            throw new CustomException(ErrorCode.NOT_THE_AUTHOR);
+        }
     }
 }
