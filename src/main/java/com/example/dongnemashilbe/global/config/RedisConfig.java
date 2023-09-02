47bff654a3180815bc8456685e1260db6d7be77a
codegyeon <99954094+codegyeon@users.noreply.github.com>
2023-09-02 14:52:43 +0900

    Develop (#82)
    * feat : 해당 유저의 리뷰리스트 조회 기능 (#80)
    
    * feat : 레디스 인기순최근순 조회 (#81)
    
    Co-authored-by: junyoung93 <jun@bagjun-yeong-ui-MacBookAir.local>
    
    ---------
    
    Co-authored-by: junyoung <128367271+junyoung93@users.noreply.github.com>
    Co-authored-by: junyoung93 <jun@bagjun-yeong-ui-MacBookAir.local>

--- src/main/java/com/example/dongnemashilbe/global/config/RedisConfig.java
+++ src/main/java/com/example/dongnemashilbe/global/config/RedisConfig.java
@@ -4,12 +4,16 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisPassword;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 @RequiredArgsConstructor
@@ -38,4 +42,13 @@ public RedisConnectionFactory redisConnectionFactory() {
         return redisTemplate;
     }
 
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(1));  // 10분
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+                .cacheDefaults(cacheConfig)
+                .build();
+    }
 }
